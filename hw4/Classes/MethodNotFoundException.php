3d8295d067a31722f3de531e8d49ff07dee6c3de
Dmitrii Kazaiev <dmytrokazaev@gmail.com>
2024-12-12 19:17:00 +0200

    hw4

---
+++ hw4/Classes/MethodNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Classes;
+
+use Exception;
+
+class MethodNotFoundException extends Exception
+{
+    public function __construct($method, $code = 0, Exception $previous = null)
+    {
+        $message = "Method $method does not exist.";
+        parent::__construct($message, $code, $previous);
+    }
+}
